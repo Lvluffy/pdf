7143bfdd01548a360cee833e87be4a7738d3d4c4
lvlufei <lvlufei@aoji.cn>
2018-12-28 18:41:56 +0800

    优化

--- app/src/main/java/com/luffy/pdf/BaseApplication.java
+++ app/src/main/java/com/luffy/pdf/BaseApplication.java
@@ -3,6 +3,7 @@ package com.luffy.pdf;
 import android.app.Application;
 
 import com.lzy.okhttputils.OkHttpUtils;
+import com.lzy.okhttputils.cache.CacheMode;
 import com.lzy.okhttputils.cookie.store.PersistentCookieStore;
 
 /**
@@ -18,6 +19,7 @@ public class BaseApplication extends Application {
         /**初始化下载框架*/
         OkHttpUtils.init(this);
         OkHttpUtils.getInstance()
+                .setCacheMode(CacheMode.NO_CACHE)                           //全局的缓存模式(不使用缓存)
                 .setConnectTimeout(OkHttpUtils.DEFAULT_MILLISECONDS)        //全局的连接超时时间
                 .setReadTimeOut(OkHttpUtils.DEFAULT_MILLISECONDS)           //全局的读取超时时间
                 .setWriteTimeOut(OkHttpUtils.DEFAULT_MILLISECONDS)          //全局的写入超时时间
